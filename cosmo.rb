5642815df792ea2f6d910e4fece50b7f85118b93
Justin Welenofsky <welenofsky@gmail.com>
2017-05-11 16:21:14 -0700

    Use class for options - Basic help message

--- cosmo.rb
+++ cosmo.rb
@@ -1,14 +1,42 @@
 #!/usr/bin/env ruby
 require 'optparse'
+require 'pp'
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
+class OptparseCosmo
 
-  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-    options[:verbose] = v
-  end
-end.parse!
+  ACTIONS = ['init', 'deploy', 'provision']
 
-p options
-p ARGV
+  def self.parse(args)
+
+    options = {}
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: example.rb <command> [options]"
+      opts.separator ""
+      opts.separator "Where command is one of the following:"
+      opts.separator "  init\t\t- Start a new project"
+      opts.separator "  deploy\t- Update existing project source code"
+      opts.separator "  provision\t- Setup environment"
+
+      action = args[0]
+      unless ACTIONS.index(action)
+        puts opts
+        exit
+      end
+
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        options[:verbose] = v
+      end
+
+    end
+
+    opt_parser.parse!(args)
+    options
+
+  end # self.parse
+
+end # OptparseCosmo
+
+options = OptparseCosmo.parse(ARGV)
+
+pp options
+pp ARGV
