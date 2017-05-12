13403b37eeb53c80cb163bedb96fb3077b43ed98
Justin Welenofsky <welenofsky@gmail.com>
2017-05-12 15:17:51 -0700

    Start init

--- cosmo.rb
+++ cosmo.rb
@@ -4,16 +4,20 @@ require 'pp'
 
 class Cosmo
 
-  DEPENDENCIES = ['python2.7', 'pip']
+  DEPENDENCIES = ['python2.7', 'pip', 'vagrant']
 
   def initialize(*args)
     # TODO stub
     # pp args
+    # exit
+
+    @options = args[0]
 
     # Verify user has required programs to continue
     DEPENDENCIES.each do |dep|
       unless command?(dep)
         puts "Can not find #{dep}. Please install it to continue"
+        puts "https://www.vagrantup.com/downloads.html" if dep == 'vagrant'
         exit
       end
     end
@@ -39,19 +43,47 @@ class Cosmo
                  'install ansible')
     end
 
+    unless FileTest.exist?(File.expand_path('~/.vagrant.d/insecure_private_key'))
+      puts "Could not find vagrant private key, unable to deploy to vagrant. Exiting..."
+      exit
+    end
+
+    send(@options[:command])
+
   end
 
   # Used to call correct playbook or to edit config files.
   def init
     # TODO stub
+    puts "Configuring project..."
+
+    # Inventory File
+    loop do
+      puts "What environment are you setting up? Example: local, prd:"
+      @inventory = STDIN.gets.chomp
+      break unless @inventory.empty?
+    end
+
+    unless File.exist?("config/#{@inventory}")
+      # Create file
+
+    end
+
   end
 
   def deploy
-    # TODO stub
+    # --limit=#{@env} ??
+    `ansible-playbook -i #{@inventory} #{@playbook} --private-key=~/.vagrant.d/insecure_private_key`
+    unless $!.success?
+      puts "Deploying project failed! Check above for more descriptive error message"
+    end
   end
 
   def provision
-    # TODO stub
+    `ansible-playbook -i #{@inventory} #{@playbook} --private-key=~/.vagrant.d/insecure_private_key`
+    unless $!.success?
+      puts "Provisioning project failed! Check above for more descriptive error message"
+    end
   end
 
   # Helper Functions
